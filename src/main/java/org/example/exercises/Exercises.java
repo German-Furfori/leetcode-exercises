228b99d3672aa600f92004c9b9527bbb77b02642
Germán Furfori <german.furfori@paygoal.com.ar>
2025-07-22 11:24:52 +0200

    Repeated String from HackerRank

--- src/main/java/org/example/exercises/Exercises.java
+++ src/main/java/org/example/exercises/Exercises.java
@@ -22,4 +22,27 @@ public class Exercises {
         return new int[] {};
     }
 
+    /**
+     * Repeated String
+     * https://www.hackerrank.com/challenges/repeated-string/problem
+     * */
+    public static long repeatedString(String s, long n) {
+        long repeats = n / s.length();
+        long module = n % s.length();
+        long count = 0;
+        long total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') count += 1;
+        }
+
+        total = repeats * count;
+
+        for (int i = 0; i < module; i++) {
+            if (s.charAt(i) == 'a') total++;
+        }
+
+        return total;
+    }
+
 }
--- src/test/java/org/example/exercises/ExercisesTest.java
+++ src/test/java/org/example/exercises/ExercisesTest.java
@@ -3,6 +3,8 @@ package org.example.exercises;
 import org.example.MainTests;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ExercisesTest extends MainTests {
 
     /**
@@ -14,6 +16,16 @@ public class ExercisesTest extends MainTests {
         this.assertArrayValues(new int[] {0, 1}, Exercises.twoSum(new int[] {2,7,11,15}, 9));
         this.assertArrayValues(new int[] {1, 2}, Exercises.twoSum(new int[] {3,2,4}, 6));
         this.assertArrayValues(new int[] {0, 1}, Exercises.twoSum(new int[] {3,3}, 6));
+    }
+
+    /**
+     * Repeated String
+     * https://www.hackerrank.com/challenges/repeated-string/problem
+     * */
+    @Test
+    public void testRepeatedString() {
+        assertEquals(7L, Exercises.repeatedString("aba", 10));
+        assertEquals(1000000000000L, Exercises.repeatedString("a", 1000000000000L));
     }
 
 }
